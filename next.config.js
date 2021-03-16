c30fdad4004d731bc760eaceb9d4899473ecf3ba
promehedi <mmhasan563@gmail.com>
2021-03-16 15:54:28 +0600

    Fix Deploy Issue

--- next.config.js
+++ next.config.js
@@ -15,5 +15,16 @@ module.exports = (phase) => {
       mongoUrl:
         'mongodb+srv://mehedi:Mehedi2020@cluster0.wrrej.mongodb.net/nextBlog?authSource=admin&replicaSet=atlas-2kzvot-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true',
     },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      config.node = {
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty',
+      }
+      return config
+    },
+    webpackDevMiddleware: (config) => {
+      return config
+    },
   }
 }
