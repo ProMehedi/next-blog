6c4696bfcded67c58f2574ac16442ea67f4b69b1
promehedi <mmhasan563@gmail.com>
2021-03-16 12:56:16 +0600

    Adding the contact API route

---
+++ pages/api/contact.js
@@ -0,0 +1,24 @@
+export default (req, res) => {
+  if (req.method === 'POST') {
+    const { name, email, message } = req.body
+
+    if (
+      !email ||
+      !email.includes('@') ||
+      !name ||
+      name.trim() === '' ||
+      !message ||
+      message.trim() === ''
+    ) {
+      res.status(422).json({ message: 'Invalid input' })
+      return
+    }
+
+    const newMessage = { id: Date.now().toString(), name, email, message }
+    console.log(newMessage)
+
+    res
+      .status(201)
+      .json({ message: 'Successfully send your message!', message: newMessage })
+  }
+}
