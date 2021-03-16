7f37737d84d016662ed02d7974c776c9b4635520
promehedi <mmhasan563@gmail.com>
2021-03-16 12:49:27 +0600

    Preparing Contact Form

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,30 @@
+import styles from '../styles/Contact.module.css'
+
 const ContactPage = () => {
   return (
-    <div>
-      <h1>Contact Page</h1>
-    </div>
+    <section className={styles.contact}>
+      <h1>How can I help you?</h1>
+      <form className={styles.form}>
+        <div className={styles.controls}>
+          <div className={styles.control}>
+            <label htmlFor='name'>Your Name</label>
+            <input type='text' id='name' required />
+          </div>
+          <div className={styles.control}>
+            <label htmlFor='name'>Your Email</label>
+            <input type='email' id='email' required />
+          </div>
+        </div>
+        <div className={styles.control}>
+          <label htmlFor='message'>Your Message</label>
+          <textarea type='email' id='message' rows='5' required></textarea>
+        </div>
+
+        <div className={styles.actions}>
+          <button>Send Message</button>
+        </div>
+      </form>
+    </section>
   )
 }
 
